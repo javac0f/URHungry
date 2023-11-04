d9f34fe83ec0059df2215488c2fcb8e433358cd9
javac0f <jcof28@gmail.com>
2023-11-04 14:35:03 -0400

    More color styling

---
+++ src/engine.py
@@ -0,0 +1,12 @@
+from taipy import Gui
+from pages import home_page
+
+
+
+
+
+if __name__ == "__main__":
+    #Core().run()
+    #scenario = tp.create_scenario(scenario_cfg)
+    Gui(page = home_page, css_file = './styling.css').run(use_reloader=True)
+
