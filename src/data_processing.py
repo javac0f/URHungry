dd7b4c03c25aea06b08da444277f5327d72ab41f
John Nguyen <nguyenduy167@yahoo.com>
2023-11-04 14:44:20 -0400

    update data_processing

--- src/data_processing.py
+++ src/data_processing.py
@@ -13,6 +13,13 @@ payment = {
     "CVV/CVC" : "000",
 }
 
+store = {
+    "name" : "walmart", # primary key
+    "min_order" : 35.00, # in dollars
+    "min_order_fee" : 7.95,
+    "fees" : 7.85,
+}
+
 user = {
     "user_id" : 1234, # primary key
     "user_name" : "luna",
@@ -24,14 +31,14 @@ item = {
     "name" : "potato",
     "quantity" : 3,
     "quote_price" : 3.5, # in dollars
-    ""
+    "alternatives" : [],
 }
 
 order = {
     "id" : 1154, # primary key
     "order_id" : 1254656,
     "user_id" : 1234,
-    "store" : "walmart", # store to order from
+    "store" : store, # store to order from
     "item" : item,
     "delivery_by_date" : "12/05/2023", # the date by which the order must arrive
 }
@@ -51,6 +58,23 @@ def pull_order(store, by_date, *orders):
     
 
 def order_summary(orders):
-    display 
+    # the orders have the same store
+    tax_rate = 0.08
+    display = {
+        "subtotal" : 0,
+        "taxes and fees" : 0,
+    }
+    subtotal = 0
     for order in orders:
-        order
+        item = order["item"]
+        subtotal += (item["quantity"]*item["quote_price"])
+    display["subtotal"] = subtotal
+    store = order[0]["store"]
+    fees = store["fees"]
+    if subtotal < store["min_order"]:
+        fees += store["min_order_fee"]
+    taxes_and_fees = subtotal*tax_rate
+    display["taxes and fees"] = taxes_and_fees
+    return display
+
+
