2669913c5d408d2ef91472180409cfbe6a20d2ec
lnhtrn <72944083+lnhtrn@users.noreply.github.com>
2023-11-05 01:39:06 -0400

    update

--- src/engine.py
+++ src/engine.py
@@ -26,7 +26,6 @@ def nagivate_to_order(state):
 
 
 
-
 # ORDER PAGE FUNCTIONS
 
 # Handling data
@@ -40,6 +39,7 @@ items_df = pd.read_csv("database/items.csv", index_col="ID")
 # Configure logger
 logging.basicConfig(format="\n%(asctime)s\n%(message)s", level=logging.INFO, force=True)
 
+MAX_REQUEST = 20
 
 def error_too_many_requests(state):
     """Notify user that too many requests have been made."""
@@ -51,10 +51,20 @@ def error_too_many_requests(state):
 # Define functions
 def sort_by_date_store(store, sort_by_date=True):
     orders = orders_df
+
+    print(orders)
+
+    # check if the store requested even have any order:
+    if store not in list(orders["StoreName"]):
+        return []
+
+    # get all order with this store name 
     orders = orders[orders["StoreName"] == store]
     items = items_df
     items["Total"] = items["Price"]*items["Quantity"]
     orders["MinOrder"] = stores_df["MinOrder"][store]
+
+    # join order table with the sum based on order ID 
     orders["Total"] = items.groupby('OrderID').sum()["Total"]
     orders["Percentage"] = orders["Total"]/orders["MinOrder"]*100
     orders["Completion"] = orders["Percentage"].apply(lambda x: 100 if x > 100 else x)
@@ -63,30 +73,78 @@ def sort_by_date_store(store, sort_by_date=True):
     return [(id, desc) for id, desc in zip(orders.index, orders["Descriptor"])]
 
 
-def choose_store(state):
-    """Generate Tweet text."""
-    state.tweet = ""
+def get_order_total(order_id):
+    this_order = items_df[items_df["OrderID"] == order_id]
+    this_order["Total"] = this_order["Price"]*this_order["Quantity"]
 
+    return sum(this_order["Total"])
+
+# choose store button
+def choose_store(state):
     # Check the number of requests done by the user
-    if state.n_requests >= 5:
+    if state.n_requests >= MAX_REQUEST:
         error_too_many_requests(state)
         return
     
-    # Generate the tweet
+    # Generate the order list
     state.n_requests += 1
-    state.tweet = f"<h3>Showing orders from {state.store}:</h3>"
     state.orders_list = sort_by_date_store(store=state.store)
+
+    if len(state.orders_list) > 0:
+        state.store_description = f"Showing active orders from {state.store}:"
+    else:
+        state.store_description = f"There are no current active order at this store. Please place a new order."
+
 
     # Notify the user in console and in the GUI
     logging.info(
-        state.all_store[state.store],f"Store selected: {state.store}"
+        f"Store selected: {state.store}"
+    )
+    notify(state, "success", "Store selected!")
+
+
+def create_new_order(state):
+    # Check if the user has put a name
+    if state.user_name == "":
+        notify(state, "error", "Please enter a name")
+        return
+
+    # Check the number of requests done by the user
+    if state.n_requests >= MAX_REQUEST:
+        error_too_many_requests(state)
+        return
+    
+    # generate new id for order 
+    state.order_id = int(orders_df.index[-1]) + 1
+
+    # get info of this order to pass to next page
+    state.current_order_price = 0
+    state.min_price = stores_df.loc[state.store]["MinOrder"]
+    if state.min_price < 1:
+        state.min_price = 0
+    state.more_money = state.min_price
+
+    # set up the description for next page 
+    state.order_detail_description = f"Order is at ${current_order_price}. Order needs ${more_money} to reach the minimum order price of ${min_price}."
+
+
+    # Notify the user in console and in the GUI
+    logging.info(
+        f"Creating new order with ID {state.order_id}."
     )
     notify(state, "success", "Order created!")
 
+    navigate(state, "order_detail")
+
 
 def select_order(state):
+    # Check if the user has put a name
+    if state.user_name == "":
+        notify(state, "error", "Please enter a name")
+        return
+
     # Check the number of requests done by the user
-    if state.n_requests >= 5:
+    if state.n_requests >= MAX_REQUEST:
         error_too_many_requests(state)
         return
     
@@ -94,6 +152,22 @@ def select_order(state):
     state.n_requests += 1
     state.order_id = state.order_selected[0]
     state.order_detail = state.order_selected[1]
+
+    # get info of this order to pass to next page
+    state.current_order_price = get_order_total(state.order_id)
+    print(get_order_total(state.order_id))
+    state.min_price = stores_df.loc[state.store]["MinOrder"]
+    if state.min_price < 1:
+        state.min_price = 0
+
+    # check amount of money needed to reach minimum order 
+    state.more_money = state.min_price - state.current_order_price
+    if state.more_money <= 0:
+        state.more_money = 0
+
+    # set up the description for next page 
+    state.order_detail_description = f"Order is at ${current_order_price}. Order needs ${more_money} to reach the minimum order price of ${min_price}."
+
 
     # Notify the user in console and in the GUI
     logging.info(
@@ -105,26 +179,18 @@ def select_order(state):
 
 
 # Variables
-tweet = ""
-order_tweet = ""
+user_name = ""
+
+store_description = ""
 order_selected = None
 order_id = ""
 order_detail = ""
 orders_list = []
-all_store = {
-    "Wegmans": {
-        "2332": "October 31 - 100%",
-        "2739": "October 21 - 26%"
-    },
-    "Target": {
-        "4788": "October 23 - 100%",
-        "4272": "October 15 - 26%"
-    },
-    "Walmart": {
-        "7020": "October 20 - 100%",
-        "7292": "October 11 - 26%"
-    },
-}
+
+current_order_price = 0
+more_money = 0
+min_price = 0
+order_detail_description = ""
 
 n_requests = 0
 
@@ -137,6 +203,43 @@ def on_exception(state, function_name: str, ex: Exception):
 
 
 
+
+
+
+
+
+
+# ORDER DETAIL PAGE
+
+item_name = ""
+item_price = ""
+item_quantity = ""
+
+def append_item(new_item):
+    items_df = items_df.append(pd.DataFrame([new_item], columns=items_df.columns), ignore_index=True)
+
+
+def add_item_to_order(state):
+    # check if empty:
+    if state.item_name == "" or state.item_price == "" or state.item_quantity == "":
+        notify(state, "error", "Please enter all the fields needed.")
+        return
+
+    # New list for append into DataFrame
+    new_item = [state.user_name, state.item_name, state.item_price, state.item_quantity, state.order_id]
+
+    # Using append to add the list to DataFrame
+    append_item(new_item)
+    
+    state.item_name = ""
+    state.item_price = ""
+    state.item_quantity = ""
+
+    # Notify the user in console and in the GUI
+    logging.info(
+        new_item
+    )
+    notify(state, "success", "New item added!")
 
 
 
--- src/pages.py
+++ src/pages.py
@@ -1,3 +1,5 @@
+#  markdown element of the pages
+
 root_md="## UR**Hungry**{:.color-secondary}"
 
 
@@ -10,7 +12,7 @@ home_page = """
 <br/>
 <br/>
 
-<center><h3>**Josh**{:.color-secondary} is ordering from **Wegmans**{:.color-secondary}</h3></center>
+<center><h3>Start ordering **now**{: .color-secondary}!</h3></center>
 
 <center><|ORDER|button|class_name = .taipy-button|on_action=nagivate_to_order|></center>\n
 
@@ -23,7 +25,13 @@ order_page = """
 <br/>
 <br/>
 
-<center><h3>Select from **store**{: .color-secondary}:</h3></center>
+<user_name|
+<center><|{user_name}|input|label=Your name (or PseudoName)|></center>
+|user_name>
+
+<br/>
+
+<center><h3>... is ordering from **store**{: .color-secondary}:</h3></center>
 
 <center><|{store}|selector|lov={stores_list}|dropdown|></center>
 
@@ -33,11 +41,15 @@ order_page = """
 
 <br/>
 
+<center><|{store_description}|text|></center>
+
 <center><|{order_selected}|selector|lov={orders_list}|></center>
 
 <center><|Show order selected|button|class_name = .taipy-button|on_action=select_order|label=Show order selected|></center>
 
-<center><|{order_tweet}|text|></center>
+<br/>
+
+<center><|Start new order|button|class_name = .taipy-serious-button|on_action=create_new_order|label=Start new order|></center>
 
 """
 
@@ -48,6 +60,37 @@ order_detail_page = """
 <br/>
 
 <center><h3>Add to **order**{: .color-secondary}:</h3></center>
+
+<br/>
+
+<center><|{order_detail_description}|text|></center>
+
+<br/>
+<br/>
+
+<|layout|columns=1 1 1|gap=30px|class_name=card|
+
+<item_name|
+<|{item_name}|input|label=Item name|>
+|item_name>
+
+<item_price|
+<|{item_price}|input|label=Item price|>
+|item_price>
+
+<item_quantity|
+<|{item_quantity}|input|label=Item quantity|>
+|item_quantity>
+
+|>
+
+<br/>
+<br/>
+
+<center><|Add item|button|class_name = .taipy-button|on_action=add_item_to_order|label=Add item|></center>
+
+<br/>
+<br/>
 
 <center><|Request items|button|class_name = .taipy-button|on_action=confirm_request_items|label=Request items|></center>
 
